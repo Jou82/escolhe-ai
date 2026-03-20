9c74f76671c5c4f27660e945251f8a786170c6c9
Douglas Reis <douglas.reis65@gmail.com>
2026-03-20 11:20:45 -0300

    correção do routes que estava impactando no login

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,15 @@
 Rails.application.routes.draw do
-  get "sessions/index"
-  get "sessions/show"
-  get "sessions/new"
-  get "movies/index"
-  get "movies/show"
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :sessions, only: [:new, :create, :show, :index]
-  resources :movies, only: [:index, :show]
-  resources :likes, only: [:create]
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  get "profile", to:"pages#profile", as: :profile
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root to: "pages#home"
+
+  resources :sessions, only: [:new, :create, :show, :index]
+  resources :movies,   only: [:index, :show]
+  resources :likes,    only: [:create]
+
+  get "up" => "rails/health#show", as: :rails_health_check
+  get "profile", to: "pages#profile", as: :profile
 end
+fqq

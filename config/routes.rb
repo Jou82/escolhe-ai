7d8c8243c72fbc20c7e85d5ae039c80bbab54112
Joana Dias <joana.jou@gmail.com>
2026-03-20 16:06:12 -0300

    a função de recomendação já está aparecendo na tela

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :show, :index]
   resources :movies,   only: [:index, :show]
   resources :likes,    only: [:create]
+  resources :movies, only: [:index, :show, :create]
 
   get "up" => "rails/health#show", as: :rails_health_check
   get "profile", to: "pages#profile", as: :profile
 end
-fqq
+

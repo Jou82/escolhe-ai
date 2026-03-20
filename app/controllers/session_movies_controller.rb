21f67e081b895b2cd0e35ddd3348f8747110c591
matheuspereirafx <matheuspereirafx@gmail.com>
2026-03-20 17:42:13 -0300

    ruby link_to e image_tag home e show sessions moview

--- app/controllers/session_movies_controller.rb
+++ app/controllers/session_movies_controller.rb
@@ -2,7 +2,7 @@ class SessionMoviesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    #@session = Session.find(params[:session_id])
+    @session = Session.find(params[:session_id])
     #@movie   = Movie.find(params[:id])
     #@genres  = @movie.genres
     #@like    = Like.find_by(session: @session, movie: @movie)

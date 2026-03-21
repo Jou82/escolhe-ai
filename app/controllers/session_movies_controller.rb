152eb279f8726523bcaaed2cf966aae432da08e0
Joana Dias <joana.jou@gmail.com>
2026-03-21 14:50:10 -0300

    visualição pronta

--- app/controllers/session_movies_controller.rb
+++ app/controllers/session_movies_controller.rb
@@ -2,9 +2,12 @@ class SessionMoviesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    #@session = Session.find(params[:session_id])
-    #@movie   = Movie.find(params[:id])
-    #@genres  = @movie.genres
-    #@like    = Like.find_by(session: @session, movie: @movie)
+    @session_record = current_user.sessions.find(params[:session_id])
+    @movie = Movie.find(params[:id])
+    @genres = @movie.genres
+    @like = Like.find_by(session: @session_record, movie: @movie)
+
+    recommendations = JSON.parse(@session_record.recommendations_data || "[]")
+    @rec_data = recommendations.find { |r| r["title"] == @movie.title }
   end
 end

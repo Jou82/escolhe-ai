2d8bc50becd8c40e43b41cf7f0496f59edd53699
matheuspereirafx <matheuspereirafx@gmail.com>
2026-03-23 16:15:58 -0300

    avatar user front, js e backend

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,12 @@ class PagesController < ApplicationController
 
   def update_profile
     platforms = params[:streaming_platforms] || []
-    current_user.update(streaming_platforms: platforms.reject(&:blank?))
+    avatar = params.dig(:user, :avatar)
+
+    attrs = { streaming_platforms: platforms.reject(&:blank?) }
+    attrs[:avatar] = avatar if avatar.present?
+
+    current_user.update(attrs)
     render json: { success: true }
   end
 end

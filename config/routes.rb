6c96678846cb5dea176b387921ed8a37e10690c2
matheuspereirafx <matheuspereirafx@gmail.com>
2026-03-24 19:23:04 -0300

    form e rotas para alterar senha na pagina user

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks'
-  }
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }, skip: [:registrations]
+
+ devise_scope :user do
+    post   '/users',         to: 'users/registrations#create'
+    patch  '/users',         to: 'users/registrations#update'
+    put    '/users',         to: 'users/registrations#update'
+    delete '/users',         to: 'users/registrations#destroy'
+    get    '/users/cancel',  to: 'users/registrations#cancel', as: 'cancel_user_registration'
+    get    '/users/sign_up', to: 'users/registrations#new',  as: 'new_user_registration'
+  end
 
   root to: "pages#home"
   resources :sessions, only: [:new, :create, :show, :index] do
     resources :movies, only: [:show], controller: "session_movies"
   end
-  resources :movies, only: [:index, :show, :create]  # ← :create aqui
+  resources :movies, only: [:index, :show, :create]
   resources :likes, only: [:create]
 
   get "up" => "rails/health#show", as: :rails_health_check

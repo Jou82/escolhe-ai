1ca4e9154a402e996a9a88f8facc682456ca0e4c
Paulo Coelho <pahdcpc@gmail.com>
2026-03-25 12:56:43 +0000

    alteracoes nova homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   def home
+    api_key = ENV.fetch('TMDB_API_KEY', nil)
+    url = URI("https://api.themoviedb.org/3/movie/popular?api_key=#{api_key}&language=pt-BR&page=1")
+    response = Net::HTTP.get(url)
+    data = JSON.parse(response)
+    @popular_movies = data["results"].first(20).sample(3).map { |m| m["title"] }
+  rescue
+    @popular_movies = ["Um Sonho de Liberdade", "O Poderoso Chefão", "O Cavaleiro das Trevas", "O Senhor dos Anéis", "A Lista de Schindler"]
   end
 
   def profile

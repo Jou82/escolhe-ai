b320c5ae95f3265a4db0ae4c9b32566d28dd8443
Douglas Reis <douglas.reis65@gmail.com>
2026-03-26 16:01:20 -0300

    fix: resolve route conflict and password reset redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  protected
+
+  def after_sending_reset_password_instructions_path_for(resource_name)
+    root_path
+  end
+
+
 end

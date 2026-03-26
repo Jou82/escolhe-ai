fbe3600f125a7a8e8be938923562558b5d3ba887
Paulo Coelho <pahdcpc@gmail.com>
2026-03-26 13:22:46 +0000

    salvar nome perfil

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_23_165622) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_26_123605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -233,6 +233,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_23_165622) do
   create_table "users", force: :cascade do |t|
     t.string "avatar"
     t.datetime "created_at", null: false
+    t.string "display_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "provider"
